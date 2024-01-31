386633bfa274e5af6ab5c379a6da3c02411c9de8
ManavDaggula <manavdaggula@gmail.com>
2024-01-31 23:55:19 +0530

    nav styles updated

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -5,16 +5,13 @@ import styles from "./Navbar.module.scss"
 function Navbar(props) {
   
   return (
-    <nav className={styles.nav}>
+    <nav className={styles.nav} style={{"--status-color":props.electionStatus ? "green" : "red"}}>
         <Link to="/"><span>Logo</span></Link>
         <ul>
             <li><Link to="/voter">Vote</Link></li>
-            <li><Link to="/admin">Admin</Link></li>
-            <li>{
-              props.electionStatus ? 
-                <span className={`${styles.circle} ${styles.green}`}></span> 
-                :
-                <span className={`${styles.circle} ${styles.red}`}></span>}</li>
+            {props.isChairperson ? <li><Link to="/admin">Admin</Link></li> : <></>}
+            {/* <li><span className={`${styles.circle} ${props.electionStatus ? styles.green : styles.red}`}></span></li> */}
+            <li style={{filter: "blur(2px)"}}><span className={styles.circle}></span></li>
         </ul>
     </nav>
   )
