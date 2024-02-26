a7eef418a4481d65f5a910e5b36bf139efa23a0a
ManavDaggula <manavdaggula@gmail.com>
2024-02-26 10:20:15 +0530

    slight updates

--- truffle/migrations/1_deploy_contracts.js
+++ truffle/migrations/1_deploy_contracts.js
@@ -1,5 +1,8 @@
-const Ballot = artifacts.require("Ballot");
+// const Ballot = artifacts.require("Ballot");
+// const ElectoralPoll = artifacts.require("ElectoralPoll")
+const ElectoralCommittee = artifacts.require("ElectoralCommittee")
 
 module.exports = function (deployer) {
-  deployer.deploy(Ballot);
+  // deployer.deploy(Ballot);
+  deployer.deploy(ElectoralCommittee);
 };
